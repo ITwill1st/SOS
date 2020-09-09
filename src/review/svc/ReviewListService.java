4aea803c54b32aa1e8548b88f37781b3ae8a6bfb
82103 <82103@DESKTOP-6FVH2SO>
2020-09-09 18:09:42 +0900

    리뷰 체커 0일때만 리뷰하도록 수정 - 동현

--- src/review/svc/ReviewListService.java
+++ src/review/svc/ReviewListService.java
@@ -14,60 +14,63 @@ import vo.ProductInfoBean;
 public class ReviewListService {
 
 	public ArrayList<OrderDTO> getOrderList(int mem_num) {
-			
+
 		Connection con = getConnection();
-		
+
 		ReviewDAO rda = ReviewDAO.getInstance();
-		
+
 		rda.setConnection(con);
-		
+
 		ArrayList<OrderDTO> orderList = rda.getOrderList(mem_num);
 
 		close(con);
-		
+
 		return orderList;
 	}
 
 	public ProductBean getProduct(int item_num) {
-		
+
 		ProductBean productBean = null;
-		
+
 		Connection con = getConnection();
-		
+
 		ReviewDAO rda = ReviewDAO.getInstance();
-		
+
 		rda.setConnection(con);
-		
+
 		productBean = rda.getProduct(item_num);
-		
+
 		close(con);
-		
+
 		return productBean;
 	}
-	
+
 	public ArrayList<ProductBean> getProduct_list(int mem_num) {
-		
+
 		ArrayList<ProductInfoBean> order_info = null;
 		ArrayList<ProductBean> product_list = new ArrayList<ProductBean>();
 		ArrayList<OrderDTO> orderList = getOrderList(mem_num);
-		
-		for(int i = 0 ; i < orderList.size() ; i++) {
+
+		for (int i = 0; i < orderList.size(); i++) {
 			OrderDTO orderDTO = orderList.get(i);
 			StringToArrayListService service_stringToArrayList = new StringToArrayListService();
-			order_info =  service_stringToArrayList.getOrderInfoArray(orderDTO);
-			
-			for(int x = 0 ; x < order_info.size() ; x++) {
-				
+			order_info = service_stringToArrayList.getOrderInfoArray(orderDTO);
+
+			for (int x = 0; x < order_info.size(); x++) {
+
 				ProductInfoBean pib = order_info.get(x);
 				int item_num = pib.getItem_num();
-				ProductBean productBean = getProduct(item_num);				
-				product_list.add(productBean);			
-				
+				int review_ck = pib.getReview_ck();
+				if (review_ck == 0) {
+					ProductBean productBean = getProduct(item_num);
+					product_list.add(productBean);
+				}
+
 			}
-		}	
+		}
 
 		return product_list;
-		
+
 	}
 
 }
