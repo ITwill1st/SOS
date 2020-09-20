4b874202645c7e7575982ab12353f3741d5870ef
82103 <82103@DESKTOP-6FVH2SO>
2020-09-20 20:30:05 +0900

    오더 테이블 수정으로 인한 리뷰 리스트 및 등록 서비스, 메서드 수정 1차 - 동현

--- src/vo/OrderDTO.java
+++ src/vo/OrderDTO.java
@@ -5,17 +5,26 @@ import java.sql.Timestamp;
 public class OrderDTO {
 	
 	int order_num;
+	int pre_num;
 	int mem_num;
-	String order_info;
+	int basket_num;
 	int table_num;
+	int item_num;
+	int item_qty;
+	int item_price;
 	Timestamp order_datetime;
-	int order_confirm;
-	
+	int review_chk;
 	public int getOrder_num() {
 		return order_num;
 	}
 	public void setOrder_num(int order_num) {
 		this.order_num = order_num;
+	}
+	public int getPre_num() {
+		return pre_num;
+	}
+	public void setPre_num(int pre_num) {
+		this.pre_num = pre_num;
 	}
 	public int getMem_num() {
 		return mem_num;
@@ -23,11 +32,11 @@ public class OrderDTO {
 	public void setMem_num(int mem_num) {
 		this.mem_num = mem_num;
 	}
-	public String getOrder_info() {
-		return order_info;
+	public int getBasket_num() {
+		return basket_num;
 	}
-	public void setOrder_info(String order_info) {
-		this.order_info = order_info;
+	public void setBasket_num(int basket_num) {
+		this.basket_num = basket_num;
 	}
 	public int getTable_num() {
 		return table_num;
@@ -35,17 +44,37 @@ public class OrderDTO {
 	public void setTable_num(int table_num) {
 		this.table_num = table_num;
 	}
+	public int getItem_num() {
+		return item_num;
+	}
+	public void setItem_num(int item_num) {
+		this.item_num = item_num;
+	}
+	public int getItem_qty() {
+		return item_qty;
+	}
+	public void setItem_qty(int item_qty) {
+		this.item_qty = item_qty;
+	}
+	public int getItem_price() {
+		return item_price;
+	}
+	public void setItem_price(int item_price) {
+		this.item_price = item_price;
+	}
 	public Timestamp getOrder_datetime() {
 		return order_datetime;
 	}
 	public void setOrder_datetime(Timestamp order_datetime) {
 		this.order_datetime = order_datetime;
 	}
-	public int getOrder_confirm() {
-		return order_confirm;
+	public int getReview_chk() {
+		return review_chk;
 	}
-	public void setOrder_confirm(int order_confirm) {
-		this.order_confirm = order_confirm;
+	public void setReview_chk(int review_chk) {
+		this.review_chk = review_chk;
 	}
+	
+	
 		
 }
